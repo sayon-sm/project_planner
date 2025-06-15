4f612949d1afc9f6237643c8f9ef70085fa7169a
sm <its.sayon.mitra@gmail.com>
2025-06-15 22:52:52 +0530

    accessing elements using DOM

---
+++ assets/scripts/app.js
@@ -0,0 +1,27 @@
+const activeProjects =
+  document.getElementById('active-projects').lastElementChild;
+const finishedProjects =
+  document.getElementById('finished-projects').lastElementChild;
+const finishCourse = document.getElementById('p1');
+const MoreInfoFC = finishCourse.querySelector('button');
+const finishFC = finishCourse.lastElementChild;
+const buyGroceries = document.getElementById('p2');
+const MoreInfoBG = buyGroceries.querySelector('button');
+const finishBG = buyGroceries.lastElementChild;
+const bookHotel = document.getElementById('p3');
+const MoreInfoBH = bookHotel.querySelector('button');
+const finishBH = bookHotel.lastElementChild;
+
+console.log(
+  activeProjects,
+  finishedProjects,
+  finishCourse,
+  MoreInfoFC,
+  finishFC,
+  buyGroceries,
+  MoreInfoBG,
+  finishBG,
+  bookHotel,
+  MoreInfoBH,
+  finishBH
+);
