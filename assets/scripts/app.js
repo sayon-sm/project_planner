bd468494aa92b3110a3970d77135e02c8197f806
sm <its.sayon.mitra@gmail.com>
2025-07-06 03:14:53 +0530

    adding eventListner to remove ToolTip

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,6 @@
 'use strict';
+
+// one single task ie. Finish the Course
 class Projects {
   constructor(id) {
     this[id] = {
@@ -8,10 +10,14 @@ class Projects {
     };
   }
 }
+
+// box that holds the active tasks
 class ActiveProjects {
   activeProjects = document.getElementById('active-projects').lastElementChild;
   componentList = [new Projects('p1'), new Projects('p2')];
 }
+
+// box that holds the finished tasks
 class FinishedProjects {
   finishedProjects =
     document.getElementById('finished-projects').lastElementChild;
@@ -22,14 +28,20 @@ class app {
   static activated = new ActiveProjects();
   static finished = new FinishedProjects();
 
+  // method to make buttons functional
   static button() {
     for (const projects in app) {
+      // to cycle through class app
       for (const arr in this[projects]) {
+        // to cycle through instance of class ActiveProjects & FinishedProjects
         const p = this[projects];
         if (Array.isArray(p[arr])) {
           for (const idx of p[arr]) {
+            // to cycle through componentList of ActiveProjects & FinishedProjects
             for (const key in idx) {
+              // to cycle through instances of class Projects
               for (const k in idx[key]) {
+                // to cycle through object named p1, p2, p3
                 const q = idx[key];
                 const button = q[k];
                 if (k === 'moreInfoButton') {
@@ -56,63 +68,30 @@ class app {
     }
   }
 
+  // functionality of More Info button
   static moreInfo() {
     const tooltip = document.createElement('div');
     tooltip.textContent = this.parentElement.dataset.extraInfo;
     tooltip.classList.add('card');
     const list = this.closest('ul');
-    list.prepend(tooltip);
-    // alert(this.parentElement.dataset.extraInfo);
+    list.append(tooltip);
+    tooltip.addEventListener('click', app.clear);
   }
 
+  static clear() {
+    this.remove();
+  }
+
+  // functionality of Finish button
   static finish(key) {
-    // const index = app.activated.componentList.findIndex(
-    //   (Projects, idx, componentList) => {
-    //     let boolean;
-    //     for (const id in Projects) {
-    //       if (id === key) {
-    //         boolean = true;
-    //       }
-    //     }
-    //     return boolean;
-    //   }
-    // );
-    // app.activated.componentList.splice(index, 1);
-    // if (app.activated.componentList.length === 0) {
-    //   app.activated.activeProjects.remove();
-    // }
-    // if (app.finished.componentList.length === 0) {
-    //   app.finished.finishedProjects.previousElementSibling.after(
-    //     app.finished.finishedProjects
-    //   );
-    // }
     app.finished.finishedProjects.appendChild(this.parentElement);
     this.textContent = 'Activate';
     this.removeEventListener('click', app.finish);
     this.addEventListener('click', app.active);
   }
 
+  // functionality of Activate button
   static active(key) {
-    // const index = app.finished.componentList.findIndex(
-    //   (Projects, idx, componentList) => {
-    //     let boolean;
-    //     for (const id in Projects) {
-    //       if (id === key) {
-    //         boolean = true;
-    //       }
-    //     }
-    //     return boolean;
-    //   }
-    // );
-    // app.finished.componentList.splice(index, 1);
-    // if (app.finished.componentList.length === 0) {
-    //   app.finished.finishedProjects.remove();
-    // }
-    // if (app.activated.componentList.length === 0) {
-    //   app.activated.activeProjects.previousElementSibling.after(
-    //     app.activated.activeProjects
-    //   );
-    // }
     app.activated.activeProjects.appendChild(this.parentElement);
     this.textContent = 'Finish';
     this.removeEventListener('click', app.active);
